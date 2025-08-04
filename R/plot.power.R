ce10576d62b67d383995c49862a3d78118b30a90
Zuchao Shen <30952210+zuchaoshen@users.noreply.github.com>
2025-08-04 09:13:07 -0400

    v 1.6.1

--- R/plot.power.R
+++ R/plot.power.R
@@ -109,7 +109,60 @@ plot.power <- function(expr, nlim = c(2, 300), plim = c(0.01, 0.99),
       }
 
   } else if (expr$funName == "od.2m"){
-    print(Jlab)
+    n = expr$out$n
+    p = expr$out$p
+    icc <- expr$par$icc
+    r12 <- expr$par$r12
+    r22m <- expr$par$r22m
+    c1 <- expr$par$c1
+    c2 <- expr$par$c2
+    c1t <- expr$par$c1t
+    omega <- expr$par$omega
+    q <- expr$par$q
+    d <- expr$par$d
+    if(is.null(m)){m <- expr$out$m}
+
+    nrange <- seq(nlim[1], nlim[2], by = 1)
+    prange <- seq(plim[1], plim[2], by = 0.01)
+    if (length(by) == 2) figure <- graphics::par(mfrow = c (1, 2))
+    if (length(by) == 1) figure <- graphics::par(mfrow = c (1, 1))
+    if ("n" %in% by) {
+      power.main <- NULL
+      for (n in nrange){
+        power.main <- c(power.main, power.2m(m = m, q = q,
+                                             p = expr$out$p, icc = icc,
+                                             r12 = r12, r22m = r22m,
+                                             d = d, c1 = c1, c2 = c2,
+                                             omega = omega,
+                                             c1t = c1t,
+                                             n = n)$out$power);
+      }
+      graphics::plot(nrange, power.main,
+                     type = "l", lty = 1,
+                     xlim = nlim, ylim = powerlim,
+                     xlab = nlab, ylab = powerlab,
+                     main = plot.title, col = "black")
+      graphics::abline(v = expr$out$n, lty = 2, col = "black")
+    }
+
+    if ("p" %in% by) {
+      power.main <- NULL
+      for (p in prange){
+        power.main <- c(power.main, power.2m(m = m, q = q,
+                                             n = expr$out$n, icc = icc,
+                                             r12 = r12, r22m = r22m,
+                                             d = d, c1 = c1, c2 = c2,
+                                             c1t = c1t,
+                                             omega = omega,
+                                             p = p)$out$power)
+      }
+      graphics::plot(prange, power.main,
+                     type = "l", lty = 1,
+                     xlim = plim, ylim = powerlim,
+                     xlab = plab, ylab = powerlab,
+                     main = plot.title, col = "black")
+      graphics::abline(v = expr$out$p, lty = 2, col = "black")
+    }
 }
 
 return(figure)
